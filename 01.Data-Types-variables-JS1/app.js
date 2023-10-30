5313aa0eaf70df0ee52f6a6bcd5a6aef676b8a38
NezrinMuxtarova <nezrinmuxtarova13@gmail.com>
2023-10-30 02:57:29 -0700

    js-1

---
+++ 01.Data-Types-variables-JS1/app.js
@@ -0,0 +1,32 @@
+//TASK 1
+
+//part 1
+
+let firstName = "Nazrin";
+let lastName = "Mukhtarova";
+let fullName;
+fullName = firstName + " " + lastName;
+console.log("fullName:", fullName);
+
+//part2
+
+let country=" Azerbaijan";
+let job="teacher";
+let age ="20";
+let personInfo=fullName + "." + " I am a " + "" + job + ". " + " I live in " + country +"." + " I am " + age + " years old.";
+console.log ( personInfo)
+
+//TASK 2
+
+let length = 16; //number
+let color = "Brown" //string
+let float = "12.5" // float
+let like = true //boolean
+let x = {
+    firstName: "Nazrin",
+    color: "blue",
+} // object
+
+
+//TASK 3
+
