963120dee503582da62e6117f5483107c23023ba
NezrinMuxtarova <nezrinmuxtarova13@gmail.com>
2023-10-31 02:44:39 -0700

    control-js2

--- 01.Data-Types-variables-JS1/app.js
+++ 01.Data-Types-variables-JS1/app.js
@@ -10,48 +10,56 @@ console.log("fullName:", fullName);
 
 //part2
 
-let country=" Azerbaijan";
-let job="teacher";
-let age ="20";
-let personInfo=fullName + "." + " I am a " + "" + job + ". " + " I live in " + country +"." + " I am " + age + " years old.";
-console.log ( personInfo)
+let country = " Azerbaijan";
+let job = "teacher";
+let age = "20";
+let personInfo =
+  fullName +
+  "." +
+  " I am a " +
+  " " +
+  job +
+  ". " +
+  " I live in " +
+  country +
+  "." +
+  " I am " +
+  age +
+  " years old.";
+console.log(personInfo);
 
 //TASK 2
 
 let length = 16; //number
-let color = "Brown" //string
-let float = "12.5" // float
-let like = true //boolean
+let color = "Brown"; //string
+let float = "12.5"; // float
+let like = true; //boolean
 let x = {
-    firstName: "Nazrin",
-    color: "blue",
-} // object
-
+  firstName: "Nazrin",
+  color: "blue",
+}; // object
 
 //TASK 3
 
-let speed ="120km/saat";
-let road ="8600km";
-let minute= speed/road ;
-let speedMatters="The car will arrive in China in" + "" + minute + "minutes.";
-console.job(sppetMatters)
+let speed = 120;
+let road = 8600;
+let minute = road / speed;
+let speedMatters = "The car will arrive in China in" + "" + minute + "minutes.";
+console.log(speedMatters);
 
+let birthYear = 2003;
+let year = 2023;
+let old = year - birthYear;
+console.log("Yaşınız:", old);
 
-let birthYear=2003;
-let year=2023;
-let old= year - birthYear;
-console.log ("Yaşınız:" , old );
-
-
-
-
-let shalala=60;
-let gulnar=90;
-let nazrin=90;
-let briliant=70;
-let shebnem=60;
-let nigar=80;
-let fidan=90;
-let guller=80;
-let average= (shalala + gulnar + nazrin + briliant + shebnem + nigar + fidan + guller)/8
-console.log("Telebelerin ortalamasi:", average)
+let shalala = 60;
+let gulnar = 90;
+let nazrin = 90;
+let briliant = 70;
+let shebnem = 60;
+let nigar = 80;
+let fidan = 90;
+let guller = 80;
+let average =
+  (shalala + gulnar + nazrin + briliant + shebnem + nigar + fidan + guller) / 8;
+console.log("Telebelerin ortalamasi:", average);
---
+++ 02.ControlStuctures-Algorithm/main.js
@@ -0,0 +1,61 @@
+//TASK 1
+
+// let number = 22;
+// if (number >= 22) {
+//   console.log("Nazik geyin");
+// } else if (number >= 15) {
+//   console.log("Pencekl geyin");
+// } else {
+//   console.log("Qalin geyin");
+// }
+
+//TASK 2
+// let letter = "k";
+
+// if (letter === "a" || letter === "e") {
+//   console.log("Sait herfdir");
+// } else if (letter === "b" || letter === "c" || letter === "d" || letter === "t") {
+//   console.log("Samit herfdir");
+// } else {
+//   console.log("Herf deyil");
+// }
+//TASK 3
+
+let a = 25;
+let b = 52;
+let c = 88;
+if (a > b && a > c) {
+  console.log(  ` ${a} is grester `);
+} else if (b > a && b > c) {
+  console.log(  `b is grester `);
+} else {
+  console.log(  `c is grester`);
+}
+
+
+//TASK 4
+
+let exam=98;
+if (exam >= 90){
+ console.log("A")
+}else if (exam >= 80){
+    console.log ("B")
+}else if(exam >=70){
+    console.log("C")
+}else if(exam >= 60){
+    console.log("D")
+} else if (exam >= 50){
+    console.log("F")
+}else{
+    console.log("Feild")
+}
+
+//TASK 5
+let firstName= "Nazrin"
+if (firstName.length<5) {
+    console.log("Salam Nazrin. Qisa adiniz var")
+}else if (firstName.length<10){
+    console.log("Salam Nazrin.Orta uzunluqlu adiniz var")
+}else{
+    console.log("Salam Nazrin.Adiniz cox uzundur")
+}
